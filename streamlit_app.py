6d64d961b9d553f5181ef51a6c39b3863dfc1ff3
Virender Prajapati <60017337+virender-prajapati@users.noreply.github.com>
2022-08-07 11:31:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom''s New Healty Diner')
+streamlit.title("My Mom's New Healty Diner")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
