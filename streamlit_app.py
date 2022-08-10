13d39bf59a26c244497b1c1be91c7fafcc27108d
Virender Prajapati <60017337+virender-prajapati@users.noreply.github.com>
2022-08-10 11:03:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title("My Mom's New Healty Diner")
 streamlit.header('Breakfast Favorites')
